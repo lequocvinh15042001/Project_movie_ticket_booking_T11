e16e9877ecebc1f25899141cd61457880878662e
lequocvinh <lequocvinh2001@gmail.com>
2022-12-10 22:11:48 +0700

    capaj nhật tính năng đặt vé

---
+++ src/pages/BookAll/Theaters/LstPhim/LstNgayChieu/index.js
@@ -0,0 +1,58 @@
+import React, { Fragment } from 'react'
+
+import formatDate from '../../../../../utilities/formatDate'
+import BtnGoToCheckOut from '../../../../../components/BtnGoToCheckOut';
+import useStyles from './style'
+
+export default function LstGioChieu({ lstLichChieuTheoPhim}) {
+  // console.log("nè",danhSachPhim);
+  const classes = useStyles()
+  
+  // const mangChiChuaNgay = lstLichChieuTheoPhim.map(item => {  // tạo mảng mới chỉ chứa ngày
+  //   return item;// item là "2020-12-17" cắt ra từ 2020-12-17T10:10:00
+  // })
+  // console.log(mangChiChuaNgay);
+
+  // const MangNgayKhongTrungLap = [...new Set(mangChiChuaNgay)] // xóa đi ngày trùng lặp > dùng mảng này để render số phần tử
+
+  // const filterByDay = (date) => { // lọc ra item từ mảng gốc
+  //   const gioChieuRenDer = lstLichChieuTheoPhim.filter(item => {
+  //     if (item.startDate.slice(0, 10) === date) {
+  //       return true
+  //     }
+  //     return false
+  //   })
+  //   return gioChieuRenDer;
+  // }
+
+  return (
+    <div className={classes.lstNgayChieu}>
+      {/* {MangNgayKhongTrungLap.map(date => (
+        <Fragment key={date}>
+          <p className={classes.ngayChieu}>{formatDate(date).dateFull}</p> {/*in ra ngày hiện tại*/}
+          <div className={classes.groupTime}>
+            {/* {lstLichChieuTheoPhim.map(lichChieuTheoPhim => ( */}
+              <Fragment key={lstLichChieuTheoPhim.id}>
+                <BtnGoToCheckOut 
+                 lichChieuTheoPhim={lstLichChieuTheoPhim.startTime}
+                 duration={lstLichChieuTheoPhim.movie?.duration}
+                 idLich={lstLichChieuTheoPhim.id}
+                 maPhim={lstLichChieuTheoPhim.movie?.id}
+                 ngayChieu={lstLichChieuTheoPhim.startDate}
+                 maPhong={lstLichChieuTheoPhim.room?.id}
+                 gioChieu={lstLichChieuTheoPhim.startTime}
+                 maRap={lstLichChieuTheoPhim.branch?.id} 
+                />
+              </Fragment>
+            {/* ))} */}
+          </div>
+
+        {/* </Fragment> */}
+      {/* )) */}
+      {/* } */}
+    </div >
+    // <div>
+    //   nulll
+    // </div>
+  )
+}
---
+++ src/pages/BookAll/Theaters/index.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useSelector } from "react-redux";
+import { useTheme } from "@material-ui/core/styles";
+
+import LstCumRap from "./LstCumRap";
+import useStyles from "./style";
+import { underLine } from "../../../styles/materialUi";
+import { colorTheater } from "../../../constants/theaterData";
+import Seperate from "../../../components/Seperate";
+import MobileLstCumrap from "./MobileLstCumrap";
+
+export default function HeThongRap() {
+  const theme = useTheme();
+  const isMobileTheater = useMediaQuery(theme.breakpoints.down("sm"));
+  const { theaterList, errorTheaterList } = useSelector(
+    (state) => state.theaterReducer
+  );
+  // console.log("Xuất hệ thống rạp: ", theaterList);
+  const [valueHeThongRap, setValueHeThongRap] = React.useState(0);
+  const classes = useStyles({ isMobileTheater, underLine });
+
+  console.log(valueHeThongRap);
+
+  if (errorTheaterList) {
+    return <div>{errorTheaterList}</div>;
+  }
+  return (
+    <div id="cumrap">
+      <Seperate />
+      <div className={classes.theater}>
+        <Tabs
+          variant={isMobileTheater ? "scrollable" : "standard"}
+          scrollButtons="on"
+          orientation={isMobileTheater ? "horizontal" : "vertical"}
+          value={valueHeThongRap}
+          classes={{ indicator: classes.tabs__indicator, root: classes.taps }}
+        >
+          {theaterList?.data?.content?.map((theater, index) => (
+            <Tab
+              onClick={() => setValueHeThongRap(index)}
+              disableRipple
+              classes={{
+                root: classes.tap,
+                textColorInherit: classes.textColorInherit,
+              }}
+              key={theater?.id}
+              label={
+                <img
+                  style={{ width: "60px", height: "60px" }}
+                  src={theater?.imgURL}
+                  alt="theaterLogo"
+                />
+              }
+            />
+          ))}
+        </Tabs>
+        {theaterList?.data?.content?.map((theater, index2) => (
+          <div
+            hidden={valueHeThongRap !== index2}
+            key={theater?.id}
+            className={classes.cumRap}
+          >
+            {isMobileTheater ? (
+              <MobileLstCumrap lstCumRap={theater} />
+            ) : (
+              <LstCumRap
+                lstCumRap={theater}
+                color={
+                  colorTheater[
+                    theater.name?.toUpperCase()
+                  ]
+                }
+                maHeThongRap={theater.id}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
