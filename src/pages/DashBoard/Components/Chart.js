7d730af062efbf53db98e70705ca75c428d384f6
lequocvinh <lequocvinh2001@gmail.com>
2022-12-24 09:00:37 +0700

    done v1

--- src/pages/DashBoard/Components/Chart.js
+++ src/pages/DashBoard/Components/Chart.js
@@ -16,20 +16,20 @@ function createData(time, amount) {
 
 const data = [
   createData("00:00", 0),
-  createData("03:00", 300),
-  createData("06:00", 600),
-  createData("09:00", 800),
-  createData("12:00", 1500),
-  createData("15:00", 2000),
-  createData("18:00", 2400),
-  createData("21:00", 2400),
+  createData("03:00", 100),
+  createData("06:00", 300),
+  createData("09:00", 200),
+  createData("12:00", 500),
+  createData("15:00", 250),
+  createData("18:00", 190),
+  createData("21:00", 730),
   createData("24:00", undefined)
 ];
 
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>Trung bình tháng này</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
@@ -43,7 +43,7 @@ export default function Chart() {
           <XAxis dataKey="time" />
           <YAxis>
             <Label angle={270} position="left" style={{ textAnchor: "middle" }}>
-              Sales ($)
+              Tần suất truy cập hệ thống
             </Label>
           </YAxis>
           <Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
