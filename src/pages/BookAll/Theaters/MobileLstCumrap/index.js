0fb8421b57002809b135210cec090e5d24c888c0
lequocvinh <lequocvinh2001@gmail.com>
2023-04-06 14:10:09 +0700

    fix

--- src/pages/BookAll/Theaters/MobileLstCumrap/index.js
+++ src/pages/BookAll/Theaters/MobileLstCumrap/index.js
@@ -16,24 +16,25 @@ function MobileLstCumRap({ lstCumRap }) {
   };
 
   return (
+    // Này là sửa item = lstCumRap
     <div className={classes.rootCumRap}>
-      {lstCumRap?.map((item, index) => (
-        <Accordion key={item.tenCumRap} style={{ direction: "ltr" }} square expanded={expanded === index} onChange={handleChange(index)}>
+      {/* {lstCumRap?.map((item, index) => ( */}
+        <Accordion key={lstCumRap?.name} style={{ direction: "ltr" }} square expanded={expanded === lstCumRap.id} onChange={handleChange(lstCumRap.id)}>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <FakeImgTheater nameTheater={item.tenCumRap} imgStyle={classes.imgTheater} />
+            <FakeImgTheater nameTheater={lstCumRap?.name} imgStyle={classes.imgTheater} />
             <div className={classes.wrapInfo} >
-              <TenCumRap tenCumRap={item.tenCumRap} testSize={12} />
-              <Address maLichChieu={item.maLichChieu} diaChiAlreadyExist={item.diaChi} />
+              <TenCumRap tenCumRap={lstCumRap?.name} testSize={12} />
+              <Address maLichChieu={lstCumRap?.id} diaChiAlreadyExist={lstCumRap?.address} />
             </div>
             <div style={{ clear: "both" }}></div>
           </AccordionSummary>
-          <AccordionDetails >
-            {(expanded === index) && item.danhSachPhim.map((phim) => (
-              <ItemPhim key={phim.tenPhim} phim={phim} />
+          {/* <AccordionDetails >
+            {(expanded === index) && item?.danhSachPhim?.map((phim) => (
+              <ItemPhim key={phim?.tenPhim} phim={phim} />
             ))}
-          </AccordionDetails>
+          </AccordionDetails> */}
         </Accordion>
-      ))}
+       {/* ))} */}
     </div>
   )
 }
