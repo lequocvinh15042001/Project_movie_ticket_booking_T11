614418627eaaf88067a0fafcdca4f81da6343628
lequocvinh <lequocvinh2001@gmail.com>
2023-06-04 13:43:18 +0200

    fix search

--- src/pages/AllMovieSearch/index.js
+++ src/pages/AllMovieSearch/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import MovieItem from "./Showtime/Desktop/MovieItem";
@@ -8,11 +8,8 @@ import moviesApi from "../../api/moviesApi";
 import Slider from "react-slick";
 import { NextArrow, PrevArrow } from "./Showtime/Desktop";
 import useStyle from "./Showtime/Desktop/style";
-import Swal from "sweetalert2";
 
 export default function AllMovieSearch() {
-  const dispatch = useDispatch();
-  const history = useHistory();
 
   const [listKetQua, setListKetQua]= useState([])
   const params = useParams()
@@ -47,7 +44,6 @@ export default function AllMovieSearch() {
   };
   return (
     <div className={classes.container}>
-      
       <Slider {...settings}>
         {listKetQua.length > 0 ?
             (listKetQua?.map((movie) => {
@@ -60,63 +56,7 @@ export default function AllMovieSearch() {
               )
           })): <h5><a href="/">❌ Không thấy, về trang chủ</a></h5>
         }
-        {/* {listKetQua.length === 0 &&  
-          Swal.fire({
-            title: "Không tìm thấy phim!",
-            icon: "warning",
-            // showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            confirmButtonText: "Về trang chủ",
-            // cancelButtonText: "Không",
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  history.push("/");
-                  window.location.reload();
-                }
-          })
-        } */}
-        {/* {listKetQua.length === 0 && 
-           Swal.fire({
-            title: "Không tìm thấy phim!",
-            icon: "warning",
-            // showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            confirmButtonText: "Về trang chủ",
-            // cancelButtonText: "Không",
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  history.push("/");
-                  window.location.reload();
-                }
-          })
-        }  */}
       </Slider>   
     </div>
   );
 }
-
-        
-          // Swal.fire({
-          //   title: "Không tìm thấy phim!",
-          //   icon: "warning",
-          //   // showCancelButton: true,
-          //   confirmButtonColor: "#3085d6",
-          //   confirmButtonText: "Về trang chủ",
-          //   // cancelButtonText: "Không",
-          //     }).then((result) => {
-          //       if (result.isConfirmed) {
-          //         history.push("/");
-          //         window.location.reload();
-          //       }
-          //     })
-          // <div style={{
-          //   left:"50%",
-          //   top:"50%",
-          //   color:"white !important",
-          //   textAlign:"center",
-          //   fontWeight:"bolder",
-          //   padding:"5rem",
-          //   position: "absolute",
-          //   transform: "translate(-50%, -50%)",
-          // }}
-          // >Không tìm thấy kết quả!</div>  
