ba6fd380624705bf387de493c0bc690eff7dc3cb
Le Quoc Vinh <lequocvinh2001@gmail.com>
2023-06-06 14:48:01 +0200

    fix

--- src/pages/Homepage/Carousel/Choose/styles.js
+++ src/pages/Homepage/Carousel/Choose/styles.js
@@ -5,7 +5,7 @@ const useStyle = makeStyles({
   search: {
     display: props => props.down992px ? "flex" : 'flex', //Hiệu ứng biến mất của tab
     // display: props => props.down992px ? "none" : 'flex', //Ẩn hiệu ứng này
-    maxWidth: "1024px",
+    maxWidth: "100%",
     // margin: "auto",
     height: "120px",
     position: "relative", //phải để là relative này mới chỉnh được, chứ absolute là bị ẩn
